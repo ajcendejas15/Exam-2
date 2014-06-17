2da9f5f6fbcc1319c5428a320bcf1404e557b8c3
ajcendejas15 <ajcendejas15@gmail.com>
2014-06-17 12:49:11 -0600

    Initial Commit

---
+++ Exam 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Exam 2
+//
+//  Created by AJ Cendejas on 6/17/14.
+//  Copyright (c) 2014 AJ Cendejas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
